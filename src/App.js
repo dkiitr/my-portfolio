4c828b3a3d29d0fd131ab2a7691ed7f1f555adb7
Deepak Kumar <dkiitr15@gmail.com>
2022-12-17 20:13:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Skills from "./components/Skills";
+import SocialLinks from "./components/SocialLinks";
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <Home />
+            <SocialLinks />
+            <About/>
+            <Portfolio/>
+            <Skills/>
+            <Contact />
+        </div>
+    );
+}
---
+++ src/components/About.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div
+            name="about"
+            className="bg-gradient-to-b from-gray-800 to-black text-white w-full h-screen"
+        >
+            <div className="mx-auto p-4 max-w-screen-lg  w-full h-full flex flex-col justify-center">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                        &lt; About /&gt;
+                    </p>
+                </div>
+                <p className="text-xl mt-20">
+                    I'm a full-stack developer located in India. I love to
+                    create simple yet beautiful websites with great user
+                    experience. I mainly work with MERN stack in my projects. I
+                    Like trying new things and building great projects.
+                </p>
+                <br />
+                <p className="text-xl">
+                    I am an Indian Institute of Technology, Roorkee graduate and
+                    being a student of one of the premier universities in India,
+                    it is my constant endeavor to match its stature and give my
+                    best to make a difference in whatever I pursue.
+                </p>
+                <br />
+                <p className="text-xl">
+                    I believe everything is an Art when you put your
+                    consciousness in it. I'm an independent freelancer and read
+                    books. I love trying new things in life. I like to travel
+                    and watch series also. You can connect with me via social
+                    links.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
